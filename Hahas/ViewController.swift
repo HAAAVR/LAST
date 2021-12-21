137c6a463c0e54d27c679b36072ad72e5d726b40
Maksim Havrilyuk <havr.maksim@gmail.com>
2021-12-21 07:48:34 +0200

    Start

---
+++ Hahas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hahas
+//
+//  Created by Максим Гаврилюк on 21.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
